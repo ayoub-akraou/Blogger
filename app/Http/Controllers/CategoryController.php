bce1b35e175a7d5c99a629577d1033af93016a2d
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-03 21:00:50 +0000

    implementer la methode show pour creer une nouvelle category avec la gestion des exceptions

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -47,7 +47,11 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        try {
+            return response()->json(['category' => $category]);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
