f5d861be921f2e7194c6812a589f86b91e96f961
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-03 17:07:57 +0000

    appliquer sanctum middleware sur la route logout

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
