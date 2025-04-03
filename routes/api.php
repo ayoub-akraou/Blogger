8759f372691b26f8873202c5c28cbd67196a8d92
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-03 16:46:43 +0000

    ajouter la route de logout dans routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
