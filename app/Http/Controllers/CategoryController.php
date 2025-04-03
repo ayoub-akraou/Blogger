e93b7d54510a1bb868294b9ef89d4c5276619c94
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-03 18:22:57 +0000

    implementer la methode index pou recuperer tous les categories avec la gestion des exceptions

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Exception;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,7 +13,12 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $categories = Category::all();
+            return response()->json(['categories' => $categories]);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
