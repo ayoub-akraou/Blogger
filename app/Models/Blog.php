f7a5f7949d6148426c831578e6d8a2af52011585
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-04 14:42:19 +0000

    creer la model Blog

---
+++ app/Models/Blog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'content',
+        'image',
+        'views',
+        'likes',
+        'status'
+    ];
+}
