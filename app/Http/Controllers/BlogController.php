7259bc51dff0a9fddea6f7e398a8672e9c6fa292
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-04 15:05:29 +0000

    ajouter la fonctionnalité de creer une nouvelle blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Blog;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class BlogController extends Controller
 {
@@ -29,7 +30,31 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'user_id' => 'required|exists:users,id',
+                'category_id' => 'nullable|exists:categories,id',
+                'title' => 'required|string|max:255',
+                'content' => 'required|string',
+                'image' => 'required|string|max:255',
+                'views' => 'required|numeric|min:0',
+                'likes' => 'required|numeric|min:0',
+                'status' => 'required|in:pending,published,archived'
+            ]);
+
+            $blog = Blog::create($request->all());
+            return response()->json(['status' => 'success', 'message' => 'Blog created successfully', 'data' => $blog]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->errors()
+            ], 422);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
@@ -37,4 +38,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('tags/{tag}', [TagController::class, 'destroy']);
     // les blogs
     Route::get('blogs', [BlogController::class, 'index']);
+    Route::post('blogs', [BlogController::class, 'store']);
 });
