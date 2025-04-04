f6bf48591076d1e9835f0722fae66ebf25a518f7
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-04 16:00:22 +0000

    creer la migration de comments

---
+++ database/migrations/2025_04_04_155642_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('blog_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('content');
+            $table->unsignedInteger('likes')->default(0);
+            $table->enum('status', ['published', 'deleted'])->default('published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
