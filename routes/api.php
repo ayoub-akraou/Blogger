da66c3b42c9e52fcc571807cb8c843d696ddab62
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-04 21:26:54 +0000

    ajouter l'endpoint DELETE  'users/{user}'

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('users', [UserController::class, 'index']);
     Route::get('users/{user}', [UserController::class, 'show']);
     Route::put('users/{user}', [UserController::class, 'update']);
+    Route::delete('users/{user}', [UserController::class, 'destroy']);
     // les categories
     Route::get('categories', [CategoryController::class, 'index']);
     Route::post('categories', [CategoryController::class, 'store']);
