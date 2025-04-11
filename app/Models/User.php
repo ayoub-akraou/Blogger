a3fc99e427d98a71ec9f6038535ff7f7d99849b6
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-12 00:41:40 +0100

    Supprimer le mot-clé abstract pour éviter les problèmes d’instanciation.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-abstract class User extends Authenticatable
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
