6743f117099a8fb67b63e807b72b5d979ccbbb67
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-12 00:45:08 +0100

    creer des utilisateurs avec differentes types

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
+use App\Models\Author;
+use App\Models\Regular;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        Author::create([
+            'name' => 'Author',
+            'email' => 'author@author.com',
+            'password' => bcrypt('password'),
+        ]);
+        
+        Regular::create([
+            'name' => 'Regular',
+            'email' => 'regular@regular.com',
+            'password' => bcrypt('password'),
+        ]);
+
     }
 }
