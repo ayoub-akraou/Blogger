4afcc509f99f097668e5e4251a5e013974b03a0c
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-12 00:35:02 +0100

    modifier la methode booted() pour que Author represente seulement les utilisateurs avec le type ="author"

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,16 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Author extends User
 {
     use HasFactory;
     protected $table = 'users';
+
     protected static function booted()
     {
-        static::creating(function ($model) {
-            $model->type = 'author';
+        static::addGlobalScope('author', function (Builder $builder) {
+            $builder->where('type', 'author');
         });
     }
+
 }
