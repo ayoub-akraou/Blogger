f2c576792cf4a459bab14ea895b0f5bb5cb61dca
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-12 13:47:29 +0100

    creer la methode register dans la modele User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -62,5 +63,14 @@ class User extends Authenticatable
         $model->setRawAttributes((array) $attributes, true);
 
         return $model;
+    }
+
+    public static function register(array $data)
+    {
+        return static::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
     }
 }
