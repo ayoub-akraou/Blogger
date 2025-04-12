9b2567ac447e024eadd41e193e1eee9d11de90d9
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-12 16:24:26 +0100

    implementer getBlogs() pour recuperer tous les articles du platforme

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -14,5 +15,21 @@ class Admin extends User
         static::addGlobalScope('admin', function (Builder $builder) {
             $builder->where('type', 'admin');
         });
+    }
+
+    public function getBlogs()
+    {
+        try {
+            $blogs = Blog::all();
+            return response()->json([
+                'status' => 'success',
+                'data' => $blogs
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
