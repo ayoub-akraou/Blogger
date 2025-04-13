177ecd63f606b63ba070700431379c5cc7a36b76
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-13 16:39:22 +0100

    creer Blog factory

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Blog;
+
+class BlogFactory extends Factory
+{
+    protected $model = Blog::class;
+
+    public function definition()
+    {
+        return [
+            'user_id' => Author::inRandomOrder()->first()->id,
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraphs(3, true),
+            'image' => $this->faker->imageUrl(800, 600),
+            'status' => $this->faker->randomElement(['active', 'suspended']),
+            'views' => $this->faker->numberBetween(0, 10000),
+            'likes' => $this->faker->numberBetween(0, 1000),
+        ];
+    }
+}
