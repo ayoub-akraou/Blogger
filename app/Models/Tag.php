aa9062206d1cfd5d4519090f47cb45f2ca7762c5
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-16 12:20:54 +0100

    implementer la methode createMultiple() au niveau de model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Tag extends Model
 {
@@ -14,7 +15,34 @@ class Tag extends Model
         'color'
     ];
 
-    public function blogs() {
+    public function blogs()
+    {
         return $this->belongsToMany(Blog::class, 'blog_tag', 'tag_id', 'blog_id');
+    }
+
+    public static function createMultiple(array $tags)
+    {
+        $existingNames = Tag::pluck('name')->toArray();
+
+        $insertData = [];
+
+        foreach ($tags as $tag) {
+            if (!in_array($tag['name'], $existingNames)) {
+                $insertData[] = [
+                    'name' => $tag['name'],
+                    'color' => $tag['color'],
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ];
+            }
+        }
+
+        if (empty($insertData)) {
+            return [];
+        }
+
+        DB::table('tags')->insert($insertData);
+
+        return Tag::whereIn('name', array_column($insertData, 'name'))->get();
     }
 }
