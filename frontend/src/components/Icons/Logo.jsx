b151ac5dda6c3a7c9d21224fca96034c70ecb116
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-21 09:56:53 +0100

    feat: ajouter le composant d’icône Logo en SVG

---
+++ frontend/src/components/Icons/Logo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Icon({ className }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 41 40"
+      fill="none"
+    >
+      <path
+        d="M21.8141 0V8.48921C27.3037 9.35252 31.6377 14.1007 31.6377 19.8561C31.6377 26.1871 26.4369 31.3669 20.0805 31.3669C14.013 31.3669 9.10124 26.7626 8.52339 20.8633H0C0.577857 31.5108 9.24571 40 20.0805 40C31.2043 40 40.161 31.0791 40.161 20C40.161 9.4964 32.0711 1.00719 21.8141 0Z"
+        fill="#FFD050"
+      />
+      <path d="M15.0243 10.5034H0V18.9926H15.0243V10.5034Z" fill="#FFD050" />
+      <path d="M19.5027 0H0V8.34532H19.5027V0Z" fill="#FFD050" />
+    </svg>
+  );
+}
