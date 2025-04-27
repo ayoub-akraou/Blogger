65de1f2e8418965c3a9fa2e5d54b8a1c9110e6a9
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-27 11:06:27 +0100

    feat: ajout du composant icône SVG Archive

---
+++ frontend/src/components/Icons/Archive.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Archive({className}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="18"
+      viewBox="0 0 20 18"
+      fill="none"
+      className={className}
+    >
+      <path
+        d="M2.28571 4.85714V16.4286H17.7143V4.85714M7.42857 8.07143H12.5714M1 1V4.85714H19V1H1Z"
+        stroke="#FFD050"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
