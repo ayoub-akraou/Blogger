e3c5798eb5f55556677cff3be958c49de4e12ae9
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-27 11:07:15 +0100

    feat: ajout du composant icône SVG Eye

---
+++ frontend/src/components/Icons/Eye.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Eye() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#48b6ff"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="w-5 h-5"
+    >
+      <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+      <circle cx="12" cy="12" r="3"></circle>
+    </svg>
+  );
+}
