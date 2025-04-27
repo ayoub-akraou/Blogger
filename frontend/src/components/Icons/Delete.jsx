4c2581fa27280b076c2b2a9e4d2c68c2a35be98f
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-27 11:06:41 +0100

    feat: ajout du composant icône SVG de suppression

---
+++ frontend/src/components/Icons/Delete.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Delete({className}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ff3b30"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={className + " w-5 h-5"}
+    >
+      <path d="M3 6h18"></path>
+      <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+      <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+    </svg>
+  );
+}
