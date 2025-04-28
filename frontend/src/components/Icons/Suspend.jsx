81b6f5ddbd3bca8d3a7e001e9a82ccb19a46b3d2
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-28 12:02:54 +0100

    feat(icônes): ajouter le composant SVG Suspend

---
+++ frontend/src/components/Icons/Suspend.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Suspend({className}) {
+  return (
+    <svg className={className + " h-5 w-5 text-gray-400 hover:text-gray-600"}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+      />
+    </svg>
+  );
+}
