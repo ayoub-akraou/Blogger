4522dcb8fa3786a3aa05e7259238e7bec5cdf52c
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-28 10:27:22 +0100

    feat(tailwind): ajoute lightGray (#6d6e76) à la config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
 				primary: "#FFD050",
 				secondary: "#232536",
 				purple: '#592ea9',
-				gray: '#6d6e76',
+				lightGray: '#6d6e76',
 				lightPurple: '#f4f0f8',
 			},
 			fontFamily: {
