44fd6e8925717aaae38165606bcb2d153c06b511
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-28 11:03:40 +0100

    refactor: ajouter l'id au props

--- frontend/src/components/UI/Button/Button.jsx
+++ frontend/src/components/UI/Button/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-export default function Button({ children, className }) {
+export default function Button({ id, children, className }) {
   return (
     <button
+      id={id}
       className={`${className} uppercase px-4 py-2 text-base text-secondary font-medium hover:opacity-95 active:scale-95`}
     >
       {children}
