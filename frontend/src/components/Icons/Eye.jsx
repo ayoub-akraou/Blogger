fc3e5449d653f006344d1e21d24b422c7596902a
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-28 12:08:02 +0100

    style(icônes): ajouter classes Tailwind stroke-blue-400 et hover:stroke-blue-500 à l’icône Delete

--- frontend/src/components/Icons/Eye.jsx
+++ frontend/src/components/Icons/Eye.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Eye() {
+export default function Eye({className}) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,11 +8,11 @@ export default function Eye() {
       height="20"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="#48b6ff"
+      // stroke="#48b6ff"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      className="w-5 h-5"
+      className={className + " w-5 h-5 stroke-blue-400 hover:stroke-blue-600"}
     >
       <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
       <circle cx="12" cy="12" r="3"></circle>
