1d05083fee41c3aedc82a8996ab9caadd853c32d
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-04-29 09:15:06 +0100

    style(author-card): ajouter object-contain pour conserver le ratio de l’image

--- frontend/src/components/UI/Cards/AuthorCard.jsx
+++ frontend/src/components/UI/Cards/AuthorCard.jsx
@@ -6,7 +6,7 @@ export default function AuthorCard({className, style, image = "/images/avatar.pn
       className={`min-w-[160px] rounded-lg shadow-md flex-1 bg-yellow-50 p-6 flex flex-col items-center text-center ${className}`}
       style={style}
     >
-      <img src={image} alt={name} className="w-36 h-36 rounded-full mb-6" />
+      <img src={image} alt={name} className="w-36 h-36 rounded-full mb-6 object-contain" />
       <h3 className="font-semibold mb-2">{name}</h3>
       <p className="text-sm text-gray-500 mb-4">Content Writer</p>
       <div className="flex space-x-3">
