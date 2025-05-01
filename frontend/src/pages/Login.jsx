68b2c32d0689b23172df629ea13a0a6059cddc77
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-05-01 11:36:09 +0100

    feat: Intégration de la gestion de connexion
    - Intégration de l'API de connexion
    - Stockage des données utilisateur dans localStorage
    - Gestion des erreurs et redirection post-connexion

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,90 +1,105 @@
-import { Link } from "react-router-dom"; 
-import React from "react";
-import Logo from "../components/UI/Logo/Logo";
-import Button from "../components/UI/Button/Button";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Logo from "../components/UI/Logo/Logo.jsx";
+import Button from "../components/UI/Button/Button.jsx";
+import apiFetch from "../api/api.js";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    apiFetch("login", "POST", { email, password }, setError)
+      .then((data) => {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        localStorage.setItem('blogs', JSON.stringify(data.blogs));
+        localStorage.setItem('user-role', data.user.type);
+        localStorage.setItem('user-id', data.user.id);
+        localStorage.setItem('categories', JSON.stringify(data.categories));
+        navigate("/");
+      })
+      .catch((err) => console.error(err));
+  }
   return (
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Blogger - Sign In</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
-      <body class="min-h-screen container mx-auto">
-        <main class="flex min-h-screen ">
-          <div class="flex-1 flex flex-col items-start justify-between p-8 md:p-14 ">
-            <Link to="/"><Logo /></Link>
+    <main className="flex min-h-screen container mx-auto">
+      <div className="flex-1 flex flex-col items-start justify-between p-8 md:p-14 ">
+        <Link to="/">
+          <Logo />
+        </Link>
 
-            <div class=" w-full mx-auto flex flex-col  items-center">
-              <h1 class="text-2xl font-medium mb-8 self-start">Sign in</h1>
+        <div className=" w-full mx-auto flex flex-col  items-center">
+          <h1 className="text-2xl font-medium self-start">Sign in</h1>
+          <div className="h-8 text-red-500">{error && error}</div>
+          <form className="space-y-6  w-full" onSubmit={handleSubmit}>
+            <div className="space-y-2">
+              <label htmlFor="email" className="sr-only">
+                Email Address *
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email Address *"
+                required
+                className="w-full px-4 py-3 border border-[#d9d9d9] rounded focus:outline-none focus:ring-2 focus:ring-[#ffd050]"
+              />
+            </div>
 
-              <form class="space-y-6  w-full">
-                <div class="space-y-2">
-                  <label for="email" class="sr-only">
-                    Email Address *
-                  </label>
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Email Address *"
-                    required
-                    class="w-full px-4 py-3 border border-[#d9d9d9] rounded focus:outline-none focus:ring-2 focus:ring-[#ffd050]"
-                  />
-                </div>
+            <div className="space-y-2">
+              <label htmlFor="password" className="sr-only">
+                Password *
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password *"
+                required
+                className="w-full px-4 py-3 border border-[#d9d9d9] rounded focus:outline-none focus:ring-2 focus:ring-[#ffd050]"
+              />
+            </div>
 
-                <div class="space-y-2">
-                  <label for="password" class="sr-only">
-                    Password *
-                  </label>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="Password *"
-                    required
-                    class="w-full px-4 py-3 border border-[#d9d9d9] rounded focus:outline-none focus:ring-2 focus:ring-[#ffd050]"
-                  />
-                </div>
-
-                <div class="flex items-center">
-                  <Button type="submit" className=" bg-primary text-black">
-                    Login
-                  </Button>
-                  <a
-                    href="#"
-                    class="ml-6 text-sm text-[#232536] hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </form>
-
-              <Button className=" bg-black text-white mt-6 w-full">
-                <Link to="/signup">Create new account</Link>
+            <div className="flex items-center">
+              <Button type="submit" className=" bg-primary text-black">
+                Login
               </Button>
-            </div>
-            <div class="flex mx-auto space-x-4 text-sm text-secondary pt-8">
-              <a href="#" class="hover:underline">
-                Terms and conditions
-              </a>
-              <a href="#" class="hover:underline">
-                Privacy policy
+              <a
+                href="#"
+                className="ml-6 text-sm text-[#232536] hover:underline"
+              >
+                Forgot your password?
               </a>
             </div>
-          </div>
+          </form>
 
-          <div class="flex-1 md:px-10 lg:px-20 hidden md:flex items-center">
-            <img
-              src="/images/login.png"
-              alt="Blogger travel inspiration"
-              class="w-full object-cover"
-            />
-          </div>
-        </main>
-      </body>
-    </html>
+          <Button className=" bg-black text-white mt-6 w-full">
+            <Link to="/signup">Create new account</Link>
+          </Button>
+        </div>
+        <div className="flex mx-auto space-x-4 text-sm text-secondary pt-8">
+          <a href="#" className="hover:underline">
+            Terms and conditions
+          </a>
+          <a href="#" className="hover:underline">
+            Privacy policy
+          </a>
+        </div>
+      </div>
+
+      <div className="flex-1 md:px-10 lg:px-20 hidden md:flex items-center">
+        <img
+          src="/images/login.png"
+          alt="Blogger travel inspiration"
+          className="w-full object-cover"
+        />
+      </div>
+    </main>
   );
 }
