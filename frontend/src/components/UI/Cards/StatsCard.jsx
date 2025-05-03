0468125a9e591ec0bfe5dd1fd970e5afd44de888
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-05-03 16:25:58 +0100

    fix: correction des classes Tailwind dans StatsCard
    - Remplacement de class par className
    - Ajout d'une bordure légère avec border border-gray-50
    - Augmentation de l'ombre avec shadow-md

--- frontend/src/components/UI/Cards/StatsCard.jsx
+++ frontend/src/components/UI/Cards/StatsCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function StatsCard({className, title, count, children}) {
   return (
-    <div className={`bg-white rounded-lg p-6 flex items-start justify-between shadow-sm ${className}`}>
+    <div className={`bg-white rounded-lg p-6 flex items-start justify-between border border-gray-50 shadow-md ${className}`}>
       <div>
-        <h3 class="text-gray-500 font-medium">{title}</h3>
-        <p class="text-3xl font-bold mt-2">{count}</p>
+        <h3 className="text-gray-500 font-medium">{title}</h3>
+        <p className="text-3xl font-bold mt-2">{count}</p>
       </div>
-      <div class="icon p-3 rounded-full">
+      <div className="icon p-3 rounded-full">
        {children}
       </div>
     </div>
