e192fa039e60592bf3ddf28bdcfae43f312c9d27
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-05-04 22:20:28 +0100

    refactor(logout): supprimer la journalisation des données lors de la déconnexion
    - Retire l'instruction `console.log(data)` inutile dans la chaîne `.then()` de la fonction [handleLogout]

--- frontend/src/components/UI/Button/LogoutButton.jsx
+++ frontend/src/components/UI/Button/LogoutButton.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import Button from './Button';
-import { Link, useNavigate } from 'react-router-dom';
-import { LogOut } from 'lucide-react';
-import apiFetch from '../../../api/api';
+import React from "react";
+import Button from "./Button";
+import { Link, useNavigate } from "react-router-dom";
+import { LogOut } from "lucide-react";
+import apiFetch from "../../../api/api";
 
 export default function LogoutButton({ className }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleLogout() {
-        console.log('logout');
-        
-        apiFetch(
-            `logout`,
-            "POST",
-            null
-        )
-          .then((data) => {
-            console.log(data);
-            localStorage.clear()
-          })
-          .catch((err) => console.error(err));
-        navigate('/login');
-    }
+  function handleLogout() {
+    console.log("logout");
+
+    apiFetch(`logout`, "POST", null)
+      .then(() => localStorage.clear())
+      .catch((err) => console.error(err));
+    navigate("/login");
+  }
 
   return (
-    <Button onClick={handleLogout} type="button" className={"flex gap-2 text-sm bg-secondary !text-white sm:!text-secondary sm:bg-white " + className}>
-        <span>Logout</span> <LogOut  className='w-5 h-5'/>
+    <Button
+      onClick={handleLogout}
+      type="button"
+      className={
+        "flex gap-2 text-sm bg-secondary !text-white sm:!text-secondary sm:bg-white " +
+        className
+      }
+    >
+      <span>Logout</span> <LogOut className="w-5 h-5" />
     </Button>
   );
 }
