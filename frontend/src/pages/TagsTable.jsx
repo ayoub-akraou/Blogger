f262dcb764acccc55d6a40cc864c2255e7676527
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-05-04 22:37:06 +0100

    feat(tags-table): implémenter l'affichage et la suppression des tags
    - Charge et affiche les tags depuis localStorage.
    - Ajoute la fonctionnalité de suppression de tag via API, avec mise à jour de l'état et de localStorage.
    - Rend le bouton de suppression fonctionnel pour chaque tag.
    - Affiche les tags dynamiquement et formate la date de création.
    - Intègre les alertes pour les retours d'action.
    - Retire la pagination statique.

--- frontend/src/pages/TagsTable.jsx
+++ frontend/src/pages/TagsTable.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Delete from "../components/Icons/Delete.jsx";
 import Cheveron from "../components/Icons/Cheveron.jsx";
 import Button from "../components/UI/Button/Button.jsx";
+import Alert from "../components/UI/Alerts/Alert.jsx";
+import apiFetch from "../api/api.js";
 
 export default function TagsTable() {
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [tags, setTags] = useState(
+    JSON.parse(localStorage.getItem("tags")) || []
+  );
+
+  function handleDelete(e) {
+    e.preventDefault();
+    const id = e.target.closest("tr").id;
+    apiFetch(`tags/${id}`, "DELETE", null, setError)
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+        setError(null);
+        const updatedTags = tags.filter((tag) => tag.id !== Number(id));
+        setTags(updatedTags);
+        localStorage.setItem("tags", JSON.stringify(updatedTags));
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setError(err.message);
+        setMessage(null);
+      });
+  }
+
   return (
-    <>
+    <div className="max-w-4xl mx-auto">
+      {error && (
+        <Alert type="error" message={error} onClose={() => setError(null)} />
+      )}
+      {message && (
+        <Alert
+          type="success"
+          message={message}
+          onClose={() => setMessage(null)}
+        />
+      )}
       <div className="flex justify-between mb-2">
         <h1 className="text-2xl font-bold text-gray-800">Tags</h1>
         <Button className="bg-primary leading-none ">Create New</Button>
       </div>
 
       {/* <!-- Table --> */}
-      <div className="bg-white rounded-md md:rounded-lg shadow overflow-hidden">
-        <table className="min-w-full divide-y divide-gray-200">
+      <div className="bg-white rounded-md md:rounded-lg shadow overflow-hidden border border-gray-300">
+        <table className="min-w-full divide-y divide-gray-300">
           <thead className="bg-white">
             <tr>
               <th
@@ -61,19 +98,24 @@ export default function TagsTable() {
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            <Row />
-            <Row />
-            <Row />
-            <Row />
-            <Row />
-            <Row />
-            <Row />
-            <Row />
+            {tags.map((tag) => {console.log(tag)
+              return <Row
+
+                key={tag?.id}
+                id={tag?.id}
+                name={tag?.name}
+                color={tag?.color}
+                createdAt={tag?.created_at}
+                blogs={tag?.blogs}
+                views={tag?.views}
+                handleDelete={handleDelete}
+              />}
+            )}
           </tbody>
         </table>
       </div>
       <Pagination />
-    </>
+    </div>
   );
 }
 
@@ -85,25 +127,36 @@ function Row({
   createdAt = "2023-01-01",
   blogs = "10",
   views = "1000",
+  handleDelete,
 }) {
   return (
-    <tr className={className}>
+    <tr className={className} id={id}>
       <td className="px-4 text-center py-2 md:px-6 md:py-4 whitespace-nowrap text-sm text-gray-500">
         {id}
       </td>
       <td className="px-4 text-center py-2 md:px-6 md:py-4 whitespace-nowrap text-sm font-medium text-gray-900">
         {name}
       </td>
-      <td className="px-4 text-center py-2 md:px-6 md:py-4 whitespace-nowrap text-sm font-medium" style={{ color: color }}>
+      <td
+        className="px-4 text-center py-2 md:px-6 md:py-4 whitespace-nowrap text-sm font-medium"
+        style={{ color: color }}
+      >
         {color}
       </td>
       <td className="px-4 text-center py-2 md:px-6 md:py-4 whitespace-nowrap text-sm text-gray-500">
-        {createdAt}
+        {new Date(createdAt).toLocaleString("en-US", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        })}
       </td>
       <td className="px-4 text-center py-2 md:px-6 md:py-4 whitespace-nowrap">
         <span
           className={`px-2 py-1 inline-flex text-xs leading-5 font-semibold rounded-full`}
-        >
+        >{console.log(blogs, views)
+        }
           {blogs}
         </span>
       </td>
@@ -115,7 +168,7 @@ function Row({
         </span>
       </td>
       <td className="px-4 text-center py-2 md:px-6 md:py-4 whitespace-nowrap text-sm font-medium">
-        <button className="mx-auto block">
+        <button className="mx-auto block" onClick={handleDelete}>
           <Delete />
         </button>
       </td>
