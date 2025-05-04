473c13b68a86627141722e142a4ced1d54ce89e2
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-05-04 22:36:37 +0100

    feat(stats): afficher les statistiques dynamiques depuis l'API
    - Récupère les statistiques générales (utilisateurs, auteurs, blogs, vues) depuis l'API et les affiche.
    - Remplit le graphique de répartition par catégorie avec les données et couleurs dynamiques de l'API.
    - Affiche les meilleurs auteurs et les blogs les plus populaires en fonction des résultats de l'API.
    - Ajoute des liens vers les détails des blogs les plus populaires.

--- frontend/src/pages/stats.jsx
+++ frontend/src/pages/stats.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Chart from "chart.js/auto";
 import AuthorCard from "../components/UI/Cards/AuthorCard";
 import StatsCard from "../components/UI/Cards/StatsCard.jsx";
@@ -6,27 +6,38 @@ import AtSign from "../components/Icons/AtSign.jsx";
 import UsersIcon from "../components/Icons/UsersIcon.jsx";
 import BlogsIcon from "../components/Icons/BlogsIcon.jsx";
 import Eye from "../components/Icons/Eye.jsx";
+import apiFetch from "../api/api.js";
+import { Link } from "react-router-dom";
 
 export default function Stats() {
+  const [stats, setStats] = useState(null);
   const chartRef = useRef(null);
   const chartInstance = useRef(null);
+  const categories = stats?.categories;
+  console.log(categories);
+  
+  const topAuthors = stats?.top_three_authors;
+  const topBlogs = stats?.top_three_blogs;
+
+  useEffect(() => {
+    apiFetch("admin/statistics", "GET", null)
+      .then((data) => {
+        setStats(data.stats);
+        setStats((prev) => {
+          return prev;
+        });
+      })
+      .catch((err) => console.error(err.message));
+  }, []);
 
   useEffect(() => {
     const ctx = chartRef.current.getContext("2d");
-
     const data = {
-      labels: ["Policy", "Economy", "IT", "Military", "Sport", "Author"],
+      labels: categories?.map((c) => c.name),
       datasets: [
         {
-          data: [30, 25, 15, 10, 12, 8],
-          backgroundColor: [
-            "#ffd050", // Policy - jaune
-            "#ff3b30", // Economy - rouge
-            "#34c759", // IT - vert
-            "#007aff", // Military - bleu
-            "#592ea9", // Sport - violet
-            "#a2845e", // Author - marron
-          ],
+          data: categories?.map((c) => c.total_blogs),
+          backgroundColor: categories?.map((c) => c.color),
           borderWidth: 0,
           hoverOffset: 10,
         },
@@ -65,36 +76,52 @@ export default function Stats() {
       options,
     });
     return () => chartInstance.current.destroy();
-  }, []);
+  }, [stats]);
 
   return (
     <>
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-6xl mx-auto">
         {/* <!-- Stats Cards --> */}
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 mb-4">
           {/* <!-- Total User --> */}
-          <StatsCard title="Total User" count={31690} className="[&_.icon]:bg-indigo-100">
+          <StatsCard
+            title="Total User"
+            count={stats?.total_users}
+            className="[&_.icon]:bg-indigo-100"
+          >
             <UsersIcon className="stroke-indigo-600" />
           </StatsCard>
           {/* <!-- Total Authors --> */}
-          <StatsCard title="Total Authors" count={40689} className="[&_.icon]:bg-pink-100">
-            <AtSign className="stroke-pink-600"/>
+          <StatsCard
+            title="Total Authors"
+            count={stats?.total_authors}
+            className="[&_.icon]:bg-pink-100"
+          >
+            <AtSign className="stroke-pink-600" />
           </StatsCard>
           {/* <!-- Total Blogs --> */}
-          <StatsCard title="Total Blogs" count={10811} className="[&_.icon]:bg-green-100">
-            <BlogsIcon className="stroke-green-600"/>
+          <StatsCard
+            title="Total Blogs"
+            count={stats?.total_blogs}
+            className="[&_.icon]:bg-green-100"
+          >
+            <BlogsIcon className="stroke-green-600" />
           </StatsCard>
 
           {/* <!-- Total Views --> */}
-          <StatsCard title="Total Views" count={32689} className="[&_.icon]:bg-yellow-100">
-            <Eye className="stroke-yellow-600"/>
+          <StatsCard
+            title="Total Views"
+            count={stats?.total_views}
+            className="[&_.icon]:bg-yellow-100"
+          >
+            <Eye className="stroke-yellow-600" />
           </StatsCard>
         </div>
 
         {/* <!-- Main Content --> */}
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
           {/* <!-- Chart Section --> */}
-          <div class="bg-white p-6 rounded-lg shadow-sm">
+          <div class="bg-white p-6 rounded-lg shadow-md border-gray-50 border">
             <h2 class="text-xl font-bold mb-6">Total Blogs By Category</h2>
             <div class="flex justify-center mb-8">
               {/* <!-- Chart.js Canvas --> */}
@@ -103,62 +130,47 @@ export default function Stats() {
               </div>
             </div>
             <div class="grid grid-cols-3 gap-4">
-              <div class="flex items-center">
-                <div class="w-6 h-6 bg-yellow-300 mr-2"></div>
-                <span>Policy</span>
-              </div>
-              <div class="flex items-center">
-                <div class="w-6 h-6 bg-red-500 mr-2"></div>
-                <span>Economy</span>
-              </div>
-              <div class="flex items-center">
-                <div class="w-6 h-6 bg-green-500 mr-2"></div>
-                <span>IT</span>
-              </div>
-              <div class="flex items-center">
-                <div class="w-6 h-6 bg-blue-500 mr-2"></div>
-                <span>Military</span>
-              </div>
-              <div class="flex items-center">
-                <div class="w-6 h-6 bg-purple-600 mr-2"></div>
-                <span>Sport</span>
-              </div>
-              <div class="flex items-center">
-                <div class="w-6 h-6 bg-amber-700 mr-2"></div>
-                <span>Author</span>
-              </div>
+              {categories?.map((category) => (
+                <div class="flex items-center">
+                  <div
+                    class="w-7 h-7 mr-1.5 rounded-full"
+                    style={{ backgroundColor: category.color }}
+                  ></div>
+                  <span className="text-sm leading-none">
+                    {category.name}: {category.total_blogs}
+                  </span>
+                </div>
+              ))}
             </div>
           </div>
 
           {/* <!-- Top Authors Section --> */}
-          <div class="space-y-6">
+          <div class="space-y-6  shadow-md border-gray-50 border">
             <div class="bg-white p-6 rounded-lg shadow-sm">
               <h2 class="text-xl font-bold mb-6">Top authors</h2>
               <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <AuthorCard className="!p-2 [&>img]:w-20 [&>img]:h-20 min-w-[120px]" />
-                <AuthorCard className="!p-2 [&>img]:w-20 [&>img]:h-20 min-w-[120px]" />
-                <AuthorCard className="!p-2 [&>img]:w-20 [&>img]:h-20 min-w-[120px]" />
+                {topAuthors?.map((author) => (
+                  <AuthorCard
+                    className="!p-2 [&>img]:w-20 [&>img]:h-20 min-w-[100px]"
+                    key={author.id}
+                    image={author.image}
+                    name={author.name}
+                  />
+                ))}
               </div>
             </div>
             {/* <!-- Top Blogs Section --> */}
             <div class="bg-white p-6 rounded-lg shadow-sm">
               <h2 class="text-xl font-bold mb-6">Top Blogs</h2>
               <div class="space-y-4">
-                {/* <!-- Blog Item 1 --> */}
-                <div class="flex justify-between items-center p-4 border rounded-lg">
-                  <div class="font-medium">Blog 1 title lorem epsilum</div>
-                  <div class="text-amber-500">1,309,182 views</div>
-                </div>
-                {/* <!-- Blog Item 2 --> */}
-                <div class="flex justify-between items-center p-4 border rounded-lg">
-                  <div class="font-medium">Blog 1 title lorem epsilum</div>
-                  <div class="text-gray-300">1,309,182 views</div>
-                </div>
-                {/* <!-- Blog Item 3 --> */}
-                <div class="flex justify-between items-center p-4 border rounded-lg">
-                  <div class="font-medium">Blog 1 title lorem epsilum</div>
-                  <div class="text-amber-500">1,309,182 views</div>
-                </div>
+                {topBlogs?.map((blog) => (
+                  <Link to={`/blog-detail/${blog.id}`} key={blog.id} className="p-4 border rounded-lg block ">
+                    <div class="flex justify-between items-center ">
+                      <div class="font-medium">{blog.title}</div>
+                      <div class="text-amber-500">{blog.views} views</div>
+                    </div>
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
