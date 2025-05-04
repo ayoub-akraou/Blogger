4ff393e1784498ce1af15da0647840e8b1ceec00
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-05-04 22:32:09 +0100

    feat(categories-page): afficher les catégories depuis l'API
    - Remplace les données statiques par un appel API pour récupérer les catégories.
    - Utilise `useState` et `useEffect` pour gérer l'état et le chargement des données.
    - Passe l'objet `category` complet au composant `CategoryCard`.
    - Affiche les catégories dynamiquement dans la grille existante.
    - Retire les logs de console.

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoryCard from "../components/UI/Cards/CategoryCard";
+import apiFetch from "../api/api";
 
 export default function Categories() {
-  const arr = Array.from({ length: 9 }, (_, i) => i + 1);
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    apiFetch("categories").then((data) => {
+      setCategories(data.categories)
+      console.log(data.categories);
+      
+    }).catch((err) => {
+      console.log(err);
+    });
+  }, []);
   return (
     <div className="container mx-auto px-4 pb-16 sm:pb-32 py-32">
       <h1 className="text-4xl font-bold text-center mb-12">
@@ -11,10 +21,10 @@ export default function Categories() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {/* <!-- Business Category --> */}
-        {arr.map((i) => (
+        {categories.map((category, i) => (
           <CategoryCard
-            image="images/new-category.png"
-            key={i}
+            category= {category}
+            key={category.id}
             className={`col-span-full ${i % 3 === 0 ? "" : "sm:!col-span-1"}`}
           />
         ))}
